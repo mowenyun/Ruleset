b991058c7405ecbb6cbd1d14ab4bf93d90d0e0fd
Matrix <152264868+Centralmatrix3@users.noreply.github.com>
2025-12-30 23:29:12 +0800

    更新 Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 rm -rf Egern Singbox
 mkdir -p Egern Singbox
 while IFS= read -r -d '' file; do
-    relative_path="${file#rule_script/rule/Clash/}"
+    relative_path="${file#ios_rule_script/rule/Clash/}"
     relative_dir="$(dirname "$relative_path")"
     mkdir -p "Egern/$relative_dir" "Singbox/$relative_dir"
     cp "$file" "Egern/${relative_path%.list}.yaml"
     cp "$file" "Singbox/${relative_path%.list}.json"
-done < <(find rule_script/rule/Clash -mindepth 1 -type f -name "*.list" -print0)
+done < <(find ios_rule_script/rule/Clash -mindepth 1 -type f -name "*.list" -print0)
 
 echo "All Ruleset Processed!"
